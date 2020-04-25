5bffc1fb244dfd0dc1f36557ae1aed3da5f66615
probabilityfilter <arunkumar@asu.edu>
2020-04-24 22:13:01 -0400

    variables names

--- p2.py
+++ p2.py
@@ -1,10 +1,8 @@
-def neuron(i,w,b):
-    o = 0
-    for x,y in zip(i,w):
-        o = o + x*y
-        print(o)
-    o = o + b
-    return(o)
+def neuron(input,weight,bias):
+    output = bias
+    for x,y in zip(input,weight):
+        output = output + x*y
+    return(output)
 
 if __name__ == '__main__':
-    print(neuron([1,2],[3,4],5))
+    print(neuron([10,2,1],[3,40,1],100))
