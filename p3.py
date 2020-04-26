ca399e373eab21683707b5b2e951b5fcf9df5cd4
probabilityfilter <arunkumar@asu.edu>
2020-04-25 23:51:09 -0400

    Take input as list

--- p3.py
+++ p3.py
@@ -1,13 +1,13 @@
 import numpy as np
 
 def layer(wts, inpt, bias):
-    if wts.shape[0] == bias.shape[0] and wts.shape[1] == inpt.shape[0]:
-        otpt = wts.dot(inpt) + bias
+    if np.array(wts).shape[0] == np.array(bias).shape[0] and np.array(wts).shape[1] == np.array(inpt).shape[0]:
+        otpt = np.dot(wts,inpt) + bias
         return(otpt)
     return("Dimension mismatch in arrays")
 
 if __name__ == '__main__':
-    img =  np.array([1,2,3,4])
-    wt =  np.array([[1,2,3,4],[5,6,7,8]])
-    b =  np.array([100,200])
+    img =  [1,2,3,4]
+    wt =  [[1,2,3,4],[5,6,7,8]]
+    b =  [100,200]
     print(type(layer(wt,img,b)), layer(wt,img,b))
