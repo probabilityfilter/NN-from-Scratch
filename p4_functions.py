8cdca9c4cf8ee5584ad681779f3db5ccda84b460
probabilityfilter <arunkumar@asu.edu>
2020-05-04 00:32:37 -0400

    lesson 4

--- p4_functions.py
+++ p4_functions.py
@@ -1,4 +1,5 @@
 import numpy as np
+np.random.seed(0)
 
 def layer(wts, inpt, bias):
     if np.array(wts).shape[0] == np.array(bias).shape[0] and np.array(wts).shape[1] == np.array(inpt).shape[0]:
@@ -11,3 +12,8 @@ if __name__ == '__main__':
     wt =  [[1,2,3,4],[5,6,7,8]]
     b =  [100,200]
     print(type(layer(wt,img,b)), layer(wt,img,b))
+
+    img2 =  [[1,2,3,4], [10,20,30,40]]
+    wt2 =  [[1,2,3,4],[5,6,7,8]]
+    b2 =  [100,200]
+    print(type(layer(wt2,img2,b2)), layer(wt2,img2,b2))
